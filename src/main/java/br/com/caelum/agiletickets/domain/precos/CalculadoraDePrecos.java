47428e85f3d7e80aa5ea5991f19e217c7edd1eed
Eduardo Geralde <eduardo_geralde@hotmail.com>
2017-02-22 22:01:19 -0300

    changed request

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -9,14 +9,15 @@ public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco = sessao.getPreco();
+		double percentualDisponivel = (sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue();
 		
 		
 		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.05) { 
+			if(percentualDisponivel <= 0.05) { 
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			} 
 		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			if((sessao.getTotalIngressos() - sessao.getIngressosReservados()) / sessao.getTotalIngressos().doubleValue() <= 0.50) { 
+			if(percentualDisponivel <= 0.50) { 
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
 			}
 			if(sessao.getDuracaoEmMinutos() > 60){
